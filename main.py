1558c0ac45db475dbba2247ed7f8c33e56b0c5b2
joshelboy <ejtberlin@gmail.com>
2022-05-04 14:02:39 +0200

    changed config.ini

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 # main -> controller -> db_controller
+
+ask_for_dbs = True
+crud_choice = ""
+dbs_choice = ""
+
+while (True):
+    if(ask_for_dbs):
+        print("Zur Auswahl stehen:")
+        print("1: Relationales Datenbanksystem (PostgreSQL)")
+        print("2: Key-Value-System (Redis)")
+        print("3: Dokumentenspeicher (MongoDB)")
+
+        dbs_choice = input("Gewähltes DBS: ")
+        ask_for_dbs = False
+
+    print("Gewähltes DBS: ")
+    print("Zur Auswahl stehen:")
+    print("1: Create")
+    print("2: Read")
+    print("3: Update")
+    print("4: Delete")
+    print("5: Anderes DBS")
+
+    crud_choice = input("Auswahl: ")
