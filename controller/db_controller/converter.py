7073765ffc8bcd8f5accbea11a8e1f376b41ac46
Benjamin <besc4087@th-wildau.de>
2022-05-06 11:46:10 +0200

    changed scheme, insert json

---
+++ controller/db_controller/converter.py
@@ -0,0 +1,37 @@
+import pymongo
+
+client = MongoClient("mongodb://localhost:27017")
+
+db = client["DB2-B1"]
+users = db["user"]
+
+def createMongoCollections(data):
+    createEmployeeTableMongoDB(data)
+    createLocationsTableMongoDB(data)
+    createJobsTableMongoDB(data)
+    createDepartmentsTableMongoDB(data)
+
+def createEmployeeTableMongoDB(data):
+    jsonFile = open('Schema/MongoDB/employee.json')
+    db.createCollection( "employees", jsonFile )
+    collection = db["employees"]
+    collection.bulk_write(data)
+
+def createLocationsTableMongoDB(data):
+    jsonFile = open('Schema/MongoDB/locations.json')
+    db.createCollection( "locations", jsonFile )
+    collection = db["locations"]
+    collection.bulk_write(data)
+
+def createJobsTableMongoDB(data):
+    jsonFile = open('Schema/MongoDB/jobs.json')
+    db.createCollection( "jobs", jsonFile )
+    collection = db["jobs"]
+    collection.bulk_write(data)
+
+def createDepartmentsTableMongoDB(data):
+    jsonFile = open('Schema/MongoDB/departments.json')
+    db.createCollection( "departments", jsonFile )
+    collection = db["departments"]
+    collection.bulk_write(data)
+
