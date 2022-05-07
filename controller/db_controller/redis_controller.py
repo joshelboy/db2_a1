971b261c2d6e91a8b35844b53fe5831812de02c0
Benjamin <besc4087@th-wildau.de>
2022-05-07 14:35:54 +0200

    redis und hotfixes

--- controller/db_controller/redis_controller.py
+++ controller/db_controller/redis_controller.py
@@ -12,20 +12,42 @@ def insertJsonToRedis(departments, employees, jobs, job_history, locations):
     r.set('job_history', job_history)
     r.set('locations', locations)
 
-def createRedis():
+def createRedis(data):
     r = redis.Redis(
         host = "localhost",
         port = 6379)
 
-    r.set()
+    insertkey = ""
+    datensatz = {}
+
+    for key, value in data:
+        if key == "table":
+            insertkey = key
+        elif key == "query":
+            for subkey, subvalue in value.items():
+                datensatz[subkey] = subvalue
+
+
+        r.set(key, datensatz)
 
     print("create")
 
-def readRedis():
+def readRedis(data):
     r = redis.Redis(
         host = "localhost",
         port = 6379)
-    print("read")
+
+    searchKey = ""
+    datensatz = {}
+
+    for key, value in data:
+        if key == "table":
+            searchKey = key
+        if key == "query":
+            for subkey, subvalue in value.items():
+                for rediskey, redisvalue in r.scan_iter():
+                    if sorted(rediskey.items()) == sorted(subkey.items()):
+                        print(datensatz)
 
 def updateRedis():
     r = redis.Redis(
@@ -33,8 +55,20 @@ def updateRedis():
         port = 6379)
     print("update")
 
-def deleteRedis():
+def deleteRedis(data):
     r = redis.Redis(
         host = "localhost",
         port = 6379)
-    print("delete")
+
+    deleteKey = ""
+    datensatz = {}
+
+    for key, value in data:
+        if key == "table":
+            deleteKey = key
+        if key == "query":
+            for subkey, subvalue in value.items():
+                for rediskey, redisvalue in r.scan_iter():
+                    if sorted(rediskey.items()) == sorted(subkey.items()):
+                        print(datensatz)
+                        r.delete(key)
